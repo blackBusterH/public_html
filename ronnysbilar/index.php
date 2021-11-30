8cc62d5b671ab0f6c3b2376796c7d3b246b9e8c5
blackBusterH <bvinjari@gmail.com>
2021-11-30 10:11:34 +0100

    small changes

--- ronnysbilar/index.php
+++ ronnysbilar/index.php
@@ -1,7 +1,23 @@
 <?php require_once('funcs.php'); ?>
 
-    <div class="landing">
+<?php get_header("header.php") ?>
 
+<main>
+
+<section class="py-5 mt-5 text-center container">
+    <div class="row mt-5 py-5">
+        <div class="col-lg-6 py-5 col-md-8 mt-5 mx-auto">
+            <h1 class="txt-white">Ronnys bilar</h1>
+            <p class="lead text-muted">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, et. Distinctio possimus fugiat facilis amet.</p>
+        </div>
+    </div>
+</section>
+
+
+    <div class="container">
+        test bla bla
     </div>
 
-    <?php get_header("header.php") ?>
+
+
+</main>
